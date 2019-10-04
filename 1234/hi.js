7155ff321fa09eaba477d80072b857371c0ecd20
Phunguyen <yolosolo25@example.com>
2019-10-04 11:36:19 +0700

    adder new line

---
+++ 1234/hi.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import {View}from 'react-native'
+
+export default class hi extends Component {
+    render() {
+        return (
+        <View>
+            hi my name aa
+        </View>
+        )
+    }
+}
