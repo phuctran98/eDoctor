06fc59cba924124196f7c82eb2bc03fab48b9ebd
phuctran98 <nguyentranphuc165@gmail.com>
2019-10-04 17:02:45 +0700

    new

--- data/datasukienn.js
+++ data/datasukienn.js
@@ -11,7 +11,7 @@ var datasukien = [
     },
     {
 
-        image : <Image source = {require('../avatar/bannerungthu.jpg')} style = {{ width : screenWidth - 20 , height : screenWidth * 666/1440 - 20,}}/>,
+        image : <Image source = {require('../avatar/2.jpg')} style = {{ width : screenWidth - 20 , height : screenWidth * 666/1440 - 20,}}/>,
         header : "Chương trình cà phê sức khỏe chủ đê Hiếu đúng và Sống khỏe với Đái tháo đường ",
         text : "Tặng miễn phí gói tầm soát tiểu đường cho khách tham dự chương trình cà phể sức khỏe cùng THs BS Diệp Thị Thanh Bình" 
     },
--- screen/CustomItem.js
+++ screen/CustomItem.js
@@ -10,27 +10,27 @@ class CustomItem extends Component {
             {/* view0 */}
                     <View style ={styles.view1}>
                         <View style={{flex:25}}>
-                            <Image style={{width:70,height:60,borderRadius:5,marginLeft:15,marginTop:15}} source={require('../avatar/doctor1.jpg')}></Image>
+                            <Image style={{width:70,height:60,borderRadius:5,marginLeft:10,marginTop:10, marginBottom : 10}} source={require('../avatar/doctor1.jpg')}></Image>
                         </View>
                         {/* image */}
-                        <View style={{flex:65,borderTopEndRadius:5,marginTop:15}}>
+                        <View style={{flex:65,borderTopEndRadius:5,marginTop:15,}}>
                             <Text style={{fontSize:14,fontWeight:'bold'}}>{this.props.xxx.ten_goi}</Text>
                             <Text style={{fontSize:14,color:'blue',fontWeight:'bold'}}>{this.props.xxx.gia_tien}</Text>
                         </View>
                     </View>
                     {/* view1 */}         
                     <View style={styles.view2}>
-                         <Text style={{marginLeft:15,marginTop:10,marginBottom:5,width:'80%'}}>{this.props.xxx.mo_ta}</Text>  
+                         <Text style={{marginLeft:10,marginTop:10,marginBottom:5,width:'80%'}}>{this.props.xxx.mo_ta}</Text>  
                     </View>
                     {/* view2 */}
 
                     <View style={styles.view3}>
-                        <View style={{flex:50,flexDirection:'row',marginTop:10,marginLeft:15}}>
+                        <View style={{flex:50,flexDirection:'row',marginTop:10,marginLeft:15, alignItems : "center"}}>
                             <Icon name='bullseye' type='font-awesome' color='blue' size={15} marginTop={2}></Icon>
                             <Text style={{fontSize:14,marginLeft:5}}>{this.props.xxx.doi_tuong}</Text>
                         </View>
                         {/* image */}
-                        <View style={{flex:50,flexDirection:'row',marginLeft:15,marginTop:10}}>
+                        <View style={{flex:50,flexDirection:'row',marginLeft:15,marginTop:10, alignItems : "center"}}>
                             <Icon name='thermometer' type='font-awesome' color='blue' size={16}></Icon>
                             <Text style={{fontSize:14,marginLeft:5}}>{this.props.xxx.so_xn}</Text>
                         </View>
@@ -45,20 +45,25 @@ class CustomItem extends Component {
 const styles = StyleSheet.create({
   
   view0: {
-    height:250,
-    width:350,
+    // height:250,
+    // width:350,
+    flex : 1,
     paddingBottom:20,
     borderRadius:5,
     marginLeft:17,
     marginTop:15,
+    marginRight : 17,
     marginBottom:10,
+
+
     backgroundColor:'white',
 
   },
 
   view1:{
-    width:350,
-    height: 95,
+    // width:350,
+    // height: 95,
+    flex: 40,
     flexDirection:'row',
     borderBottomWidth:0.3,
     borderTopStartRadius:5,
@@ -67,14 +72,16 @@ const styles = StyleSheet.create({
   },
 
   view2:{
-    height:105,
+    // height:105,
+    flex : 40,
     flexDirection:'row',
     borderBottomWidth:0.3,
     // backgroundColor:'green',
 
   },
   view3:{
-    height:50,
+    // height:50,
+    flex : 20,
     flexDirection:'row',
     borderBottomStartRadius:5,
     borderBottomEndRadius:5,
