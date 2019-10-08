bc506cc8a734dc6f8ada3d1ec91e2464b91c74ff
Phunguyen <yolosolo25@example.com>
2019-10-08 15:07:25 +0700

    new108

--- data/datagoixntq.js
+++ data/datagoixntq.js
@@ -1,19 +1,19 @@
 const datagoixntq= [
     {   
         buoc:'Bước 1: Điều dưỡng eDoctor đến tận nhà để lấy mẫu xét nghiệm và chuyển đến trung tâm y khoa Medic để tiến hành các phân tích ',
-        noi_dung:'Công thức máu, phát hiện thiếu máu, các rối loạn về máu, nhiễm trùng, bệnh bạch cầu...'
+        noi_dung:'1Công thức máu, phát hiện thiếu máu, các rối loạn về máu, nhiễm trùng, bệnh bạch cầu...'
     },
     {   
-        buoc:'Bước 1: Điều dưỡng eDoctor đến tận nhà để lấy mẫu xét nghiệm và chuyển đến trung tâm y khoa Medic để tiến hành các phân tích ',
-        noi_dung:'Công thức máu, phát hiện thiếu máu, các rối loạn về máu, nhiễm trùng, bệnh bạch cầu...'
+        buoc:'Bước 2: Điều dưỡng eDoctor đến tận nhà để lấy mẫu xét nghiệm và chuyển đến trung tâm y khoa Medic để tiến hành các phân tích ',
+        noi_dung:'2Công thức máu, phát hiện thiếu máu, các rối loạn về máu, nhiễm trùng, bệnh bạch cầu...'
     },
     {   
-        buoc:'Bước 1: Điều dưỡng eDoctor đến tận nhà để lấy mẫu xét nghiệm và chuyển đến trung tâm y khoa Medic để tiến hành các phân tích ',
-        noi_dung:'Công thức máu, phát hiện thiếu máu, các rối loạn về máu, nhiễm trùng, bệnh bạch cầu...'
+        buoc:'Bước 3: Điều dưỡng eDoctor đến tận nhà để lấy mẫu xét nghiệm và chuyển đến trung tâm y khoa Medic để tiến hành các phân tích ',
+        noi_dung:'3Công thức máu, phát hiện thiếu máu, các rối loạn về máu, nhiễm trùng, bệnh bạch cầu...'
     },
     {   
-        buoc:'Bước 1: Điều dưỡng eDoctor đến tận nhà để lấy mẫu xét nghiệm và chuyển đến trung tâm y khoa Medic để tiến hành các phân tích ',
-        noi_dung:'Công thức máu, phát hiện thiếu máu, các rối loạn về máu, nhiễm trùng, bệnh bạch cầu...'
+        buoc:'Bước 4: Điều dưỡng eDoctor đến tận nhà để lấy mẫu xét nghiệm và chuyển đến trung tâm y khoa Medic để tiến hành các phân tích ',
+        noi_dung:'4Công thức máu, phát hiện thiếu máu, các rối loạn về máu, nhiễm trùng, bệnh bạch cầu...'
     },
     
 ]
