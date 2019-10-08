96d561273fc5ac13efbd69997afde6d03f927d22
Phunguyen <yolosolo25@example.com>
2019-10-08 16:23:08 +0700

    new108

---
+++ data/datagoixntq2.js
@@ -0,0 +1,16 @@
+const datagoixntq2= [
+    {   
+        noi_dung:'1.Công thức máu, phát hiện thiếu máu, các rối loạn về máu, nhiễm trùng, bệnh bạch cầu...'
+    },
+    {   
+        noi_dung:'2.Công thức máu, phát hiện thiếu máu, các rối loạn về máu, nhiễm trùng, bệnh bạch cầu...'
+    },
+    {   
+        noi_dung:'3.Công thức máu, phát hiện thiếu máu, các rối loạn về máu, nhiễm trùng, bệnh bạch cầu...'
+    },
+    {   
+        noi_dung:'4.Công thức máu, phát hiện thiếu máu, các rối loạn về máu, nhiễm trùng, bệnh bạch cầu...'
+    },
+    
+]
+export default datagoixntq2;
--- screen/GoiXntq.js
+++ screen/GoiXntq.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import {StyleSheet,FlatList,Text,View} from 'react-native';
 import datagoixntq from '../data/datagoixntq'
+import datagoixntq2 from '../data/datagoixntq2'
 import ItemGoixntq from '../component/ItemGoixntq'
 import { Icon } from 'react-native-elements';
-// import { ScrollView } from 'react-native-gesture-handler';
+import { ScrollView } from 'react-native-gesture-handler';
 
 export default class XNTN extends React.Component {
   constructor(props) {
@@ -15,12 +16,61 @@ export default class XNTN extends React.Component {
   }
   render() {
     return (
-      
-            <FlatList 
+      <ScrollView style = {styles.viewx}>
+      {/* <ImageBackground source={require('../avatar/doctor.jpg')} style={{width: '100%', height: '100%'}}> */}
+        <View style = {styles.view0}>
+        {/* view0 */}
+                <View style ={styles.view1}>
+                    <View style={{flex:65,borderTopEndRadius:5,marginLeft:15}}>
+                        <Text style={{fontSize:18,fontWeight:'bold'}}>Gói xét nghiệm tổng quát</Text>
+                    </View>
+                    <View style={{flexDirection:'row',marginLeft:15}}>
+                        <Text style={{fontSize:14}}>Khu vực:</Text>
+                        <Text style={{fontSize:14,marginLeft:10}}>Hà Nội</Text>
+                    </View>
+                </View>
+
+                {/* view1 */}         
+                <View style={styles.view2}>
+                    <View style={{flex:50,flexDirection:'row',marginTop:10,marginLeft:15, alignItems : "center"}}>
+                        <Icon name='bullseye' type='font-awesome' color='grey' size={16}></Icon>
+                        <Text style={{fontSize:14,marginLeft:5}}>Mọi người</Text>
+                    </View>
+                    <View style={{flex:50,flexDirection:'row',marginLeft:17,marginTop:10, alignItems : "center"}}>
+                        <Icon name='thermometer' type='font-awesome' color='grey' size={16}></Icon>
+                        <Text style={{fontSize:14,marginLeft:5}}>13 hạng mục xét nghiệm</Text>
+                    </View>
+                </View>
+                {/* view2 */}
+        </View>
+        {/* ============== */}
+        <Text style={{fontSize:18,fontWeight:'bold',marginLeft:20,marginTop:25}}>Chi tiết</Text>
+        {/* ============== */}
+        <View style = {styles.view00}>
+        {/* view0 */}
+        <FlatList 
             data={datagoixntq}
-            renderItem={({item}) =><ItemGoixntq yyy={item} onPressBook={this.onPressBook.bind(this)} /> }
+            renderItem={({item}) =><ItemGoixntq yyy={item}/> }
 
             />
+             
+        </View>
+        {/* =========== */}
+        <View style={{flex:65,borderTopEndRadius:5,marginLeft:20}}>
+                            <Text style={{fontSize:15,fontWeight:'bold'}}>NỘI DUNG XÉT NGHIỆM</Text>
+                        </View>
+        {/* ============ */} 
+        <View style = {styles.view00}>
+        {/* view0 */}
+        <FlatList 
+            data={datagoixntq2}
+            renderItem={({item}) =><ItemGoixntq yyy={item} /> }
+
+            />      
+        </View>
+        {/* </ImageBackground> */}
+  </ScrollView> 
+            
       
     );
   }
@@ -28,16 +78,40 @@ export default class XNTN extends React.Component {
 
 const styles = StyleSheet.create({
   viewx:{
-    height:700,
-    width:'100%',
+    flex:1,
+    height:'100%',
     backgroundColor:'#e9f7f1'
   },
-  view_1:{
-    height: 30,
-    flexDirection:'row',
-    justifyContent:'center',
-    alignItems:'center',
-    borderBottomWidth:0.3,
+  view0: {
+    flex : 1,
+    paddingBottom:20,
+    borderRadius:5,
+    marginLeft:17,
+    marginTop:65,
+    marginRight : 17,
+    marginBottom:10,
     backgroundColor:'white',
+  },
+  view00: {
+    flex : 1,
+    paddingBottom:20,
+    borderRadius:5,
+    marginLeft:17,
+    marginRight : 17,
+    marginTop:10,
+    // backgroundColor:'white',
+  },
+
+  view1:{
+    flex: 50,
+    flexDirection:'column',
+    marginTop:10,
+    // backgroundColor:'tomato'
+  },
+  view2:{
+    flex: 50,
+    flexDirection:'column',
+    marginTop:10,
+    // backgroundColor:'green'
   },
 });
