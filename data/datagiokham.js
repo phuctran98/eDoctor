fa8c10da461e69176b8acb322909198368bd3cce
Phunguyen <yolosolo25@example.com>
2019-10-11 09:55:49 +0700

    new1110

---
+++ data/datagiokham.js
@@ -0,0 +1,23 @@
+const datagiokham= [
+    {   
+        time:'05:30',
+        buoi:'sáng'
+    },
+    {   
+        time:'06:00',
+        buoi:'sáng'
+    },
+    {   
+        time:'06:30',
+        buoi:'sáng'
+    },
+    {   
+        time:'07:00',
+        buoi:'sáng'
+    },
+    {   
+        time:'07:30',
+        buoi:'sáng'
+    },
+]
+export default datagiokham;
